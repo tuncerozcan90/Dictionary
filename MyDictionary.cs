af184e4600552db672893aa532e08d79e64665a9
Özcan Tuncer <97348094+tuncerozcan90@users.noreply.github.com>
2022-08-26 00:54:06 +0300

    Add project files.

---
+++ MyDictionary.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class MyDictionary<K, V>
+    {
+        KeyValuePair<K, V>[] items;
+
+        public MyDictionary()
+        {
+            items = new KeyValuePair<K, V>[0];
+        }
+
+        public void Add(K key, V value)
+        {
+            KeyValuePair<K, V>[] tempArray = items;
+
+            items = new KeyValuePair<K, V>[items.Length + 1];
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+            items[items.Length - 1] = new KeyValuePair<K, V>(key, value);
+        }
+
+        private bool CheckIfKeyExist(K key)
+        {
+            bool found = false;
+            foreach (var item in items)
+            {
+                if (item.Key.Equals(key))
+                {
+                    found = true;
+                }
+            }
+            return found;
+
+        }
+        public void ListItems()
+        {
+            foreach (var item in items)
+            {
+                Console.WriteLine(item.Key + " = " + item.Value);
+            }
+        }
+        public int Count
+        { 
+            get { return items.Length; }
+        }
+    }
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Dictionary
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> siraAd = new MyDictionary<int, string>();
+
+            siraAd.Add(1, "Özcan");
+            siraAd.Add(5, "Ahmet");
+            siraAd.Add(6, "Veli");
+            siraAd.Add(4, "Hasan");
+            siraAd.Add(3, "Sevda");
+            siraAd.Add(2, "Melek");
+
+
+            siraAd.ListItems();
+
+            Console.WriteLine("eleman sayısı :" +siraAd.Count);
+            Console.ReadLine();
+
+        }
+    }
+
+   
+}
